14a95ffe0379c2820441267f5b29bbe2b9e8fb3b
Anna Sedlar <annasedlar@gmail.com>
2018-01-06 23:12:40 -0500

    Update server.rb

--- server.rb
+++ server.rb
@@ -1,8 +1,12 @@
 require 'socket'
+
+# specify type of server and port
 server = TCPServer.new 3000
+
 loop do
   socket = server.accept
   socket.write "HTTP/1.0 200 OK\r\n"
+  # content-length usually required by broswers (specify bits within body)  
   socket.write "Content-Length: 2\r\n"
   socket.write "\r\n"
   socket.write "hi\n"
